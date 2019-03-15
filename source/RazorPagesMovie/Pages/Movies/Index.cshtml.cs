1b2a2fa13fc7d3412070ca4382609e296887c121
Jonas Andre Haslund <jhaslund@gmail.com>
2019-03-15 20:26:46 +0100

    Added search filters for movies. Filter on title and genre

--- source/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ source/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -19,10 +20,33 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = _context.Movie.Select(m => m);
+            var genreQuery = _context.Movie
+                .Select(m => m.Genre)
+                .Distinct()
+                .OrderBy(g => g);
+
+            if (!string.IsNullOrEmpty(SearchString))
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                var ors = MovieGenre.Split('|');
+                if (ors.Length == 1)
+                    movies = movies.Where(s => s.Genre.Equals(ors.Single()));
+                else
+                    movies = movies.Where(s => ors.Contains(s.Genre));
+            }
+
+            Genres = new SelectList(await genreQuery.ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
